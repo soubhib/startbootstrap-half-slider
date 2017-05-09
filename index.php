32a2116de2aa0ec9e049003aeef5783fe5ec5465
Don Winnie <winniedepooh666@gmail.com>
2017-05-09 11:40:25 +0000

    Made proper changes - Winnie

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="Soubhi Belhaj" content="">
+
+    <title>Veiligheidshelmstickers.nl</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Half Slider CSS -->
+    <link href="bootstrap/css/half-slider.css" rel="stylesheet">
+    
+    <!--Custom CSS-->
+    <link href="content/stylesheet/custom.css" rel="stylesheet">
+    
+
+
+</head>
+
+<body>
+
+      <div class="row demo-row">
+        <div class="col-lg-12">
+          <nav class="navbar navbar-default navbar-embossed" role="navigation">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+                <span class="sr-only">Toggle navigation</span>
+              </button>
+              <a class="navbar-brand" href="#">Veiligheidshelmstickers.nl</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+
+      <div class="row demo-row">
+        <div class="col-lg-12">
+          <nav class="navbar navbar-inverse navbar-embossed" role="navigation">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+                <span class="sr-only">Toggle navigation</span>
+              </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse-01">
+              <ul class="nav navbar-nav navbar-left">
+                <li><a href="#fakelink">Home</a>
+                <li><a href="#fakelink">Over Ons</a></li>
+                <li><a href="#fakelink">Product</a>
+                <li><a href="#fakelink">Prijzen</a></li>
+                <li><a href="#fakelink">Voorbeelden</a>x
+                <li><a href="#fakelink">Contact</a></li>
+               </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+
+
+    <!-- Half Page Image Background Carousel Header -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <!-- Set the first background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 1</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 2</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the third background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 3</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <center><h1>Half Slider by Start Bootstrap</h1></center>
+                <br />
+                    <div class="col-lg-6">
+                        <p class ="bg-warning">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Quae fere omnia appellantur uno ingenii nomine, easque virtutes qui habent, ingeniosi vocantur. 
+                            Mihi enim erit isdem istis fortasse iam utendum. Invidiosum nomen est, infame, suspectum. 
+                            Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret. 
+                            Bonum liberi: misera orbitas. Etenim semper illud extra est, quod arte comprehenditur.
+                            Moriatur, inquit. Scaevola tribunus plebis ferret ad plebem vellentne de ea re quaeri. 
+                            Duo Reges: constructio interrete. Utilitatis causa amicitia est quaesita.
+                        </p>
+                    </div>
+                    <div class="col-lg-6">
+                        <p class ="bg-success">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Quae fere omnia appellantur uno ingenii nomine, easque virtutes qui habent, ingeniosi vocantur. 
+                            Mihi enim erit isdem istis fortasse iam utendum. Invidiosum nomen est, infame, suspectum. 
+                            Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret. 
+                            Bonum liberi: misera orbitas. Etenim semper illud extra est, quod arte comprehenditur.
+                            Moriatur, inquit. Scaevola tribunus plebis ferret ad plebem vellentne de ea re quaeri. 
+                            Duo Reges: constructio interrete. Utilitatis causa amicitia est quaesita.
+                        </p>
+                    </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2017</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="bootstrap/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
